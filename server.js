fb1a297db98559f9a009d11c173a1962ad65689c
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2015-04-02 02:40:02 +0530

    removed older file

--- server.js
+++
@@ -1,9 +0,0 @@
-//HTTP Server using NodeJS
-var http = require('http');
-var reqListen = function (req,res) {
-	res.writeHead(200);
-	res.end('Hey! I am Server!!\n');
-}
-
-var server = http.createServer(reqListen);
-server.listen(8080);
